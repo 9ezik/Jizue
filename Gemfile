14da29a3eb3aa70d211f11bc6761c1cdf05c7f12
rpoltavets <poltavets228133712@gmail.com>
2023-05-16 18:15:04 +0300

    create views + page options

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.2.2"
 
+gem "breadcrumbs_on_rails"
+gem "meta-tags"
 gem "activestorage"
 gem "bootstrap-sass"
 gem "coffee-rails"
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,15 @@
 class MainController < ApplicationController
+
+  before_action :set_page_options
+
   def index
     @brands = Brand.all
     @hits = Product.all
+  end
+
+  def set_page_options
+    @page_title = 'Jizue'
+    @page_description = 'Clothes Shop'
+    @page_keywords = 'Man Woman Kid'
   end
 end
