82d987f4947ec36d725958df191349dabda07783
Nikolay00474 <nikolay00747@gmail.com>
2023-05-20 13:21:07 +0300

    add search

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 class Product < ApplicationRecord
-    enum hit: {not_hit: 0, hit: 1}
-
     has_many :galleries
     belongs_to :category
+
+    enum hit: {not_hit: 0, hit: 1}
+    enum status: {active: 1, not_active: 1 }
+
+    scope :active, -> {where(status: 1)}
 end
